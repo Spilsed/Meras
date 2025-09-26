3bedbaaf7903046dfc0d7b53a97b4467142b7f6f
Spilsed <93678295+Spilsed@users.noreply.github.com>
2025-09-26 14:59:38 -0400

    Update TopBar.svelte buttons

--- src/routes/TopBar.svelte
+++ src/routes/TopBar.svelte
@@ -11,11 +11,21 @@
 
 <div class="h-[5vh] flex justify-center items-center">
     <div class="flex items-center gap-2">
-        <button class="hover:cursor-pointer w-10" onclick={() => {}}>
+        <button
+            class="hover:cursor-pointer w-5"
+            onclick={() => {
+                month--;
+            }}
+        >
             <StepBack size="20" />
         </button>
-        <h1 class="text-center text-xl w-20">{month_name}</h1>
-        <button class="hover:cursor-pointer w-10" onclick={() => {}}>
+        <h1 class="text-center text-xl w-30">{month_name}</h1>
+        <button
+            class="hover:cursor-pointer w-5"
+            onclick={() => {
+                month++;
+            }}
+        >
             <StepForward size="20" />
         </button>
     </div>
