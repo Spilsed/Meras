84075f353bc0262307c7f7eac833c2376dfdec23
Spilsed <93678295+Spilsed@users.noreply.github.com>
2025-10-01 12:39:37 -0400

    init backend

---
+++ src-tauri/src/db.rs
@@ -0,0 +1,117 @@
+use chrono::{Datelike, TimeZone, Utc};
+use rusqlite::{params, Connection, Result};
+use serde::Serialize;
+use tauri::{AppHandle, Manager};
+use std::fs;
+
+#[derive(Debug, Serialize)]
+pub struct Event {
+    id: i32,
+    datetime: u64,
+    duration: Option<u16>,
+    title: String,
+    description: Option<String>,
+}
+
+fn get_connection(app_handle: AppHandle) -> Connection {
+    let app_dir = app_handle
+            .path()
+            .app_data_dir()
+            .expect("failed to get app dir");
+
+    fs::create_dir_all(&app_dir).unwrap();
+
+    Connection::open(format!("{}/meras.db", app_dir.display())).unwrap()
+}
+
+#[tauri::command]
+pub fn create_database(app_handle: AppHandle) -> Result<(), ()> {
+    let conn = get_connection(app_handle);
+
+    conn.execute(
+            "CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            datetime UNSIGNED BIG INT NOT NULL,
+            duration UNSIGNED INT,
+            title TEXT NOT NULL,
+            description TEXT
+        )",
+        [],
+    )
+    .unwrap();
+
+    Ok(())
+}
+
+fn query_events(app_handle: AppHandle) -> Result<Vec<Event>> {
+    let conn = get_connection(app_handle);
+
+    let mut stmt = conn.prepare("SELECT id, datetime, duration, title, description FROM events")?;
+    let event_iter = stmt.query_map([], |row| {
+        Ok(Event {
+            id: row.get(0)?,
+            datetime: row.get(1)?,
+            duration: row.get(2)?,
+            title: row.get(3)?,
+            description: row.get(4)?,
+        })
+    })?;
+
+    event_iter.collect()
+}
+
+#[tauri::command]
+pub fn insert_event(app_handle: AppHandle, datetime: u64, duration: u16, title: &str, description: &str) -> Result<()> {
+    let conn = get_connection(app_handle);
+
+    conn.execute(
+        "INSERT INTO events (datetime, duration, title, description) VALUES (?1, ?2, ?3, ?4)",
+        params![datetime, duration, title, description],
+    ).map_err(|err| err.to_string())?;
+
+    Ok(())
+}
+
+fn update_event(app_handle: AppHandle, id: i32, new_event: Event) -> Result<()> {
+    let conn = get_connection(app_handle);
+
+    conn.execute(
+        "UPDATE events SET datetime = ?2, duration = ?3, title = ?4, description = ?5 WHERE id = ?1",
+        params![
+            id,
+            new_event.datetime,
+            new_event.duration,
+            new_event.title,
+            new_event.description
+        ],
+    )?;
+
+    Ok(())
+}
+
+fn delete_event(app_handle: AppHandle, id: i32) -> Result<()> {
+    let conn = get_connection(app_handle);
+
+    conn.execute("DELETE FROM events WHERE id = ?1", params![id])?;
+
+    Ok(())
+}
+
+#[tauri::command]
+pub fn get_events_for_month(app_handle: AppHandle, month_index: u32, year: i32) -> Result<Vec<Event>, ()> {
+    let events = query_events(app_handle).unwrap();
+
+    let mut event_output: Vec<Event> = vec![];
+
+    for event in events {
+        let datetime = Utc.timestamp_opt(event.datetime as i64, 0).unwrap();
+
+        if datetime.month0() == month_index {
+            if datetime.year() == year {
+                event_output.push(event);
+            }
+        }
+    }
+
+    Ok(event_output)
+}
--- src/routes/TopBar.svelte
+++ src/routes/TopBar.svelte
@@ -3,27 +3,57 @@
 
     import { StepBack, StepForward } from '@lucide/svelte';
 
+    import { calendarState } from '$lib/state/calendar-state.svelte';
     import { month_index_to_name } from '$lib/formatting';
 
-    let month = $state(0);
-    let month_name = $derived(month_index_to_name(month));
+    let month_name = $derived(month_index_to_name(calendarState.currentDate.getMonth()));
+
+    invoke('create_database');
+
+    $effect(() => {
+        invoke('get_events_for_month', {
+            monthIndex: calendarState.currentDate.getMonth(),
+            year: calendarState.currentDate.getFullYear(),
+        }).then((events) => console.log(events));
+    });
 </script>
 
-<div class="h-[5vh] flex justify-center items-center">
+<div class="h-[5vh] flex justify-center items-center gap-2">
     <div class="flex items-center gap-2">
         <button
             class="hover:cursor-pointer w-5"
             onclick={() => {
-                month--;
+                calendarState.decrementYear();
             }}
         >
             <StepBack size="20" />
         </button>
-        <h1 class="text-center text-xl w-30">{month_name}</h1>
         <button
             class="hover:cursor-pointer w-5"
             onclick={() => {
-                month++;
+                calendarState.incrementYear();
+            }}
+        >
+            <StepForward size="20" />
+        </button>
+        <h1 class="text-xl w-15 text-right">{calendarState.currentDate.getFullYear()}</h1>
+    </div>
+    <div class="flex items-center gap-2">
+        <h1 class="text-center text-xl w-30">
+            {month_name}
+        </h1>
+        <button
+            class="hover:cursor-pointer w-5"
+            onclick={() => {
+                calendarState.decrementMonth();
+            }}
+        >
+            <StepBack size="20" />
+        </button>
+        <button
+            class="hover:cursor-pointer w-5"
+            onclick={() => {
+                calendarState.incrementMonth();
             }}
         >
             <StepForward size="20" />
